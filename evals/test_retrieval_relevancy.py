6327ceaad699c2686a1226c8ba7c50016c917dd1
farhoud <farhoud.m7@gmail.com>
2025-12-05 18:15:05 +0330

    Fix evals utils and test for async chatbot

--- evals/test_retrieval_relevancy.py
+++ evals/test_retrieval_relevancy.py
@@ -3,22 +3,25 @@ from deepeval.metrics import AnswerRelevancyMetric
 from deepeval.test_case import LLMTestCase
 
 from examples.chatbot.chatbot import chat_with_rag
+from .utils import OpenRouterLLM
 
 THRESHOLD = 0.5
 
+model = OpenRouterLLM("openai/gpt-oss-20b:free")
+
 
 @pytest.mark.parametrize(
-    "query_index", range(5)
+    "query_index", range(1)
 )  # Assuming 5 queries based on total_docs
-def test_chatbot_answer_relevancy(dataset, query_index):
+async def test_chatbot_answer_relevancy(dataset, query_index):
     _a, q, _r = dataset
 
     queries = list(q.values())
     query_data = queries[query_index]
     query = query_data["query"]
-    response = chat_with_rag(query)
+    response = await chat_with_rag(query)
 
-    metric = AnswerRelevancyMetric()
+    metric = AnswerRelevancyMetric(model=model)
     test_case = LLMTestCase(
         input=query,
         actual_output=response,
